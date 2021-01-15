6a5a1d643a8269a6ac699c8e7c6caf63a2802e25
sumin03 <breath.ing0210@gmail.com>
2021-01-15 23:22:09 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+// Handle scrolling when tapping on the navbar menu 
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener('click', (addEventListener) => {
+    console.log(event.target.dataset.link);
+    const target = event.target; 
+    const link = target.dataset.link; 
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+     console.log(scrollTo);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+})

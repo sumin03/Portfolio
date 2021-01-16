02d74f74071c3737e98ad31c96d7394c15aaa217
sumin03 <breath.ing0210@gmail.com>
2021-01-16 18:22:01 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -19,12 +19,23 @@ const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener('click', (addEventListener) => {
     console.log(event.target.dataset.link);
     const target = event.target; 
-    const link = target.dataset.link; 
+    const link = target.dataset.link ; 
     if(link == null){
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-     console.log(scrollTo);
-    scrollTo.scrollIntoView({behavior:'smooth'});
+    
+    scrollIntoView(link);
 })
+
+//Contact me = > #contact 
+
+const  homeContactBtn = document.querySelector(".home__contact"); 
+homeContactBtn.addEventListener('click', ()=> {
+   scrollIntoView('#contact');
+})
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}

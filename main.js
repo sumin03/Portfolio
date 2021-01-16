ac31d1fc5275b8f02cb2ea608c65ac96050fa221
sumin03 <breath.ing0210@gmail.com>
2021-01-16 23:00:18 +0900

    JS: transparent

--- main.js
+++ main.js
@@ -35,7 +35,33 @@ homeContactBtn.addEventListener('click', ()=> {
 })
 
 
+//Make home slowly fade to transparent as the window scrolls down 
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height; 
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:'smooth'});
 }
+
+

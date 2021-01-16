47a470c55eb3660843d816164beeb7542eec7646
sumin03 <breath.ing0210@gmail.com>
2021-01-16 23:12:56 +0900

    JS: Arrrow up

--- main.js
+++ main.js
@@ -44,9 +44,21 @@ document.addEventListener('scroll', ()=>{
 
 
 
+//Show "arrow up" button when scrolling down 
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight /2){
+        arrowUp.classList.add("visible"); 
+
+    }else{
+        arrowUp.classList.remove("visible"); 
+    }
+});
 
 
-
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
 
 
 

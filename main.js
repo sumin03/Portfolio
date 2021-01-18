262ea6df22aa0f1f93ed599cf917eb383c01d238
sumin03 <breath.ing0210@gmail.com>
2021-01-18 23:16:48 +0900

    JS: work filtering

--- main.js
+++ main.js
@@ -55,15 +55,37 @@ document.addEventListener('scroll', () => {
     }
 });
 
-
+//Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
-})
+});
 
 
+//Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project"); //원하는 프로젝트들을 다 받아오는 것! 
+workBtnContainer.addEventListener('click', (e) =>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter; 
+    if(filter == null){
+        return;
+    }
 
+    projectContainer.classList.add('anim-out');
 
-
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },300);
+    
+});
 
 
 

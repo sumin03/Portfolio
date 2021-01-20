318cff9b81c2d9942196a4b95df965d5a16c04db
sumin03 <breath.ing0210@gmail.com>
2021-01-20 19:05:12 +0900

    JS: scrolling button events

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ navbarMenu.addEventListener('click', (addEventListener) => {
     }
     navbarMenu.classList.remove('open');
     scrollIntoView(link);
+    selectNavItem(target);
 })
 
 
@@ -119,3 +120,66 @@ function scrollIntoView(selector) {
 }
 
 
+
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact',
+  ];
+  const sections = sectionIds.map(id => document.querySelector(id));
+  const navItems = sectionIds.map(id =>
+    document.querySelector(`[data-link="${id}"]`)
+  );
+  
+  let selectedNavIndex = 0;
+  let selectedNavItem = navItems[0];
+  function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+  }
+  
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  };
+  
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if (entry.boundingClientRect.y < 0) {
+          selectedNavIndex = index + 1;
+        } else {
+          selectedNavIndex = index - 1;
+        }
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  sections.forEach(section => observer.observe(section));
+
+  
+  window.addEventListener('wheel', ()=> {
+      if(window.scrollY ===0){
+          selectedNavIndex = 0; 
+      }else if (
+
+        Math.round(window.scrollY + window.innerHeight) >=
+    
+        document.body.clientHeight
+    
+      ) {
+          selectedNavIndex = navItems.length - 1; 
+      }
+      selectNavItem(navItems[selectedNavIndex]);
+  })
